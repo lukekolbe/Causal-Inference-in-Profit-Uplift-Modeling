03572334a91e36ae1803fbdca75d0b7277c309de
kolbeluk <lukas.kolbe@gmx.de>
2019-05-26 14:26:52 +0200

    Ordnerstruktur für Übersichtlichkeit

--- fashion a/fashion_a_cleaning-sampling.R
+++ fashion a/fashion_a_cleaning-sampling.R
@@ -20,7 +20,9 @@ set.seed(666)
 
 getwd()
 f_a <- read.csv("/Users/lukaskolbe/Library/Mobile Documents/com~apple~CloudDocs/UNI/Master/Applied Predictive Analytics/Data/fashion/FashionA.csv", sep=",")
-f_a <- read.csv("/Users/Lukas/Library/Mobile Documents/com~apple~CloudDocs/UNI/Master/Applied Predictive Analytics/Data/fashion/FashionA.csv", sep=",")
+#f_a <- read.csv("/Users/Lukas/Library/Mobile Documents/com~apple~CloudDocs/UNI/Master/Applied Predictive Analytics/Data/fashion/FashionA.csv", sep=",")
+f_a <- read.csv("H:\\Applied Predictive Analytics\\Data\\fashion\\FashionA.csv", sep=",")
+
 
 table(f_a$controlGroup)
 str(f_a)
@@ -39,81 +41,102 @@ prop.table(table(f_a$checkoutAmount>0, f_a$controlGroup)) #~5% have a positive c
 
 treatment_uplift_a <- aggregate(checkoutAmount ~ controlGroup, data=f_a, mean)[1,2] - aggregate(checkoutAmount ~ controlGroup, data=f_a, mean)[2,2]
 treatment_uplift_a #the treatment gives an average uplift across the whole population of 0.4892036
-summary(aov_a <- aov(checkoutAmount  ~ controlGroup, data=f_a)) # the differences in checkout amount are statistically significant!
-ttest_a <- t.test(checkoutAmount ~ controlGroup, data=f_a) # the differences in checkout amount are statistically significant!
-ttest_a
+summary(aov(checkoutAmount  ~ controlGroup, data=f_a)) # the differences in checkout amount are statistically significant!
+t.test(checkoutAmount ~ controlGroup, data=f_a) # the differences in checkout amount are statistically significant!
 
 names(f_a)
 
 table(f_a$campaignMov)
-table(f_a$campaignMov)
 
+table(f_a$checkoutAmount>=105,f_a$controlGroup) #14.800 people qualified for actually using the discount of 20€ through achieving the minimum order value
+with(f_a, prop.table(table(checkoutAmount>=105,controlGroup), margin=1)) # 25% of the treatment group have a checkout amount >=105 and 22.8% of the control group do
 
-table(f_a$checkoutAmount>=105,f_a$controlGroup)
 
 f_a$treatment <- numeric(nrow(f_a))
 f_a$treatment <- ifelse(f_a$controlGroup==1, 0, 1)
 
 f_a <- f_a[,c(4:9,94,63,10:62,64:93,1,2,3)] # sorting new for better visibility of important columns
-f_a <- f_a[,-which(names(f_a) %in% c("campaignUnit", "campaignTags", "trackerKey", "campaignId"))] #removing empty/useless factors to avoid issues with glm
 
-f_a_5 <- f_a[f_a$campaignValue==500,] #separating the different treatment values
-f_a_0 <- f_a[f_a$campaignValue==0,]
-f_a <- f_a[f_a$campaignValue==2000,] # only work with those with campaign-value of "2000" as they are the largest uniform group!
+#f_a_5 <- f_a[f_a$campaignValue==500,-which(names(f_a) %in% c("campaignUnit", "campaignTags", "trackerKey", "campaignId", "campaignValue"))] #separating the different treatment values
+#f_a_0 <- f_a[f_a$campaignValue==0,-which(names(f_a) %in% c("campaignUnit", "campaignTags", "trackerKey", "campaignId", "campaignValue"))]
+f_a <- f_a[f_a$campaignValue==2000,-which(names(f_a) %in% c("campaignUnit", "campaignTags", "trackerKey", "campaignId", "campaignValue"))] # only work with those with campaign-value of "2000" as they are the largest uniform group!
+
+
+
+# stratification for production -------------------------------------------
+
+# 
+# train_indices_f_a <- list()
+# 
+# combinations <- expand.grid(list("Conversion"=c(0,1), "Treatment"= c(0,1))) # treatment is ordered 1,0 compared to hillström data because the variable indicates control group membership
+# xtabs(~converted+controlGroup, f_a)
+# sample_size_f_a <- as.numeric(xtabs(~converted+controlGroup, f_a))
+# 
+# 
+# for(i in 1:4){
+#   train_indices_f_a[[i]] <- sample(which(f_a$converted == combinations$Conversion[i] &
+#                                            f_a$controlGroup == combinations$Treatment[i])
+#                                    , size = round(0.75*sample_size_f_a[i]), replace=FALSE) 
+# } 
+# 
+# 
+# 
+# trainIndex_f_a <- c(train_indices_f_a, recursive=TRUE)
+# 
+# trainData <- f_a[trainIndex_f_a,]
+# testData  <- f_a[-trainIndex_f_a,]
+# 
 
 # 1ST ROUND SAMPLE SPLITTING AND STRATIFICATION ---------------------------------------------------
 
 train_indices_f_a <- list()
 
 combinations <- expand.grid(list("Conversion"=c(0,1), "Treatment"= c(0,1))) # treatment is ordered 1,0 compared to hillström data because the variable indicates control group membership
-xtabs(~converted+controlGroup, f_a)
-sample_size_f_a <- as.numeric(xtabs(~converted+controlGroup, f_a))
+xtabs(~converted+treatment, f_a)
+sample_size_f_a <- as.numeric(xtabs(~converted+treatment, f_a))
 
 
 for(i in 1:4){
   train_indices_f_a[[i]] <- sample(which(f_a$converted == combinations$Conversion[i] &
-                                           f_a$controlGroup == combinations$Treatment[i])
-                                    , size = round(0.25*sample_size_f_a[i]), replace=FALSE) 
+                                           f_a$treatment == combinations$Treatment[i])
+                                   , size = round(0.4*sample_size_f_a[i]), replace=FALSE) 
 } 
 
 
 
 trainIndex_f_a <- c(train_indices_f_a, recursive=TRUE)
 
-trainData <- f_a[trainIndex_f_a,] # temporarily the train data is only a small partition!
-#testData  <- f_a[-trainIndex_f_a,]
+trainData_small <- f_a[trainIndex_f_a,] # temporarily the learning data is only a small partition!
 
 # SAMPLE SPLITTING AND STRATIFICATION 2ND ROUND (FOR PRE-TRAINING) ---------------------------------------------------
-
 
 train_indices_f_a2 <- list()
 
 #combinations_f_a2 <- expand.grid(list("Conversion"=c(0,1), "Treatment"= c(1,0))) # treatment is ordered 1,0 compared to hillström data because the variable indicates control group membership
-sample_size_f_a2 <- as.numeric(xtabs(~converted+controlGroup, trainData))
+sample_size_f_a2 <- as.numeric(xtabs(~converted+treatment, trainData_small))
 
 
 for(i in 1:4){
-  train_indices_f_a2[[i]] <- sample(which(trainData$converted == combinations$Conversion[i] &
-                                            trainData$controlGroup == combinations$Treatment[i])
-                                         , size = round(0.25*sample_size_f_a2[i]), replace=FALSE) 
+  train_indices_f_a2[[i]] <- sample(which(trainData_small$converted == combinations$Conversion[i] &
+                                            trainData_small$treatment == combinations$Treatment[i])
+                                    , size = round(0.25*sample_size_f_a2[i]), replace=FALSE) 
 } 
 
 
 
 trainIndex_f_a2 <- c(train_indices_f_a2, recursive=TRUE)
 
-trainData_f_a2 <- trainData[-trainIndex_f_a2,] # temporarily the train data is only a small partition!
-testData_f_a2  <- trainData[trainIndex_f_a2,]
+trainData_f_a2 <- trainData_small[-trainIndex_f_a2,] # temporarily the train data is only a small partition!
+testData_f_a2  <- trainData_small[trainIndex_f_a2,]
 
 table(trainData_f_a2$checkoutAmount>0, trainData_f_a2$controlGroup)
 
-summary(aov_a <- aov(checkoutAmount  ~ controlGroup, data=trainData_f_a2)) # the differences in checkout amount are STILL statistically significant!
-t.test(checkoutAmount ~ controlGroup, data=trainData_f_a2) # the differences in checkout amount are still statistically significant, but less so compared to the total population
+summary(aov(checkoutAmount  ~ controlGroup, data=trainData_f_a2)) # checking statistical significance of the differences in checkout amount
+t.test(checkoutAmount ~ controlGroup, data=trainData_f_a2) # checking statistical significance of the differences in checkout amount
 
+aggregate(checkoutAmount ~ controlGroup, data=f_a, mean)[1,2] - aggregate(checkoutAmount ~ controlGroup, data=f_a, mean)[2,2] 
 aggregate(checkoutAmount ~ controlGroup, data=trainData_f_a2, mean)[1,2] - aggregate(checkoutAmount ~ controlGroup, data=trainData_f_a2, mean)[2,2] 
-#total population uplift is no slightly lower than in the complete sample
-
-
+#total population uplift is slightly lower than in the complete sample
 
 
 
@@ -123,7 +146,7 @@ experiment <- table(list("Control" = trainData_f_a2$controlGroup, "Converted" = 
 experiment
 
 # The ATE is the outcome difference between the groups, assuming that individuals in each group are similar
-                                  # (((which is plausible because of the random sampling)))
+# (((which is plausible because of the random sampling)))
 mean(as.numeric(trainData_f_a2$converted[trainData_f_a2$controlGroup==0])) - mean(as.numeric(trainData_f_a2$converted[trainData_f_a2$controlGroup==1]))
 mean(trainData_f_a2$checkoutAmount[trainData_f_a2$controlGroup==0]) - mean(trainData_f_a2$checkoutAmount[trainData_f_a2$controlGroup==1])
 
@@ -141,18 +164,18 @@ mean(trainData_f_a2$checkoutAmount[trainData_f_a2$controlGroup==0]) - mean(train
 # In randomized empirical experiments the treatment and control groups should be roughly similar (i.e. balanced) in their distributions of covariates.
 # Of course, we would expect the conversion rate to be different between the treatment and control group
 
-    # n <- names(f_a)
-    # f <- as.formula(paste("controlGroup ~", paste(n[!n %in% c("controlGroup","converted")], collapse = " + "))) # checkBalance() throws an error with the syntax (controlGroup ~.-converted), so I save the formula separately. doesn't help.
-    # f
-    # 
-    # cb <- checkBalance(f, data = trainData_f_a2, report = c("adj.means", "adj.mean.diffs", "p.values", "chisquare.test"))
-    # 
-    # # Balance properties of first ten covariates 
-    # # Be aware that the results are saved as a tensor or '3D matrix'.
-    # dim(cb$results)
-    # round(cb$results[,,], 2)
-    # # The function automatically computes a chi-squared test for the conditional independence of the covariates to the treatment variable
-    # cb$overall
+# n <- names(f_a)
+# f <- as.formula(paste("controlGroup ~", paste(n[!n %in% c("controlGroup","converted")], collapse = " + "))) # checkBalance() throws an error with the syntax (controlGroup ~.-converted), so I save the formula separately. doesn't help.
+# f
+# 
+# cb <- checkBalance(f, data = trainData_f_a2, report = c("adj.means", "adj.mean.diffs", "p.values", "chisquare.test"))
+# 
+# # Balance properties of first ten covariates 
+# # Be aware that the results are saved as a tensor or '3D matrix'.
+# dim(cb$results)
+# round(cb$results[,,], 2)
+# # The function automatically computes a chi-squared test for the conditional independence of the covariates to the treatment variable
+# cb$overall
 
 # CARET SMOTE SAMPLING TRYOUT ---------------------------------------------
 
--- fashion b/fashion_b.R
+++ fashion b/fashion_b.R
@@ -4,7 +4,7 @@
 # install.packages("caret")
 # install_github("susanathey/causalTree")
 # install_github("saberpowers/causalLearning")
-# library(devtools) 
+# install.packages("tools4uplift")
 
 library(devtools)
 library(causalTree)
@@ -12,89 +12,183 @@ library(caret)
 library(grf)
 library("uplift")
 library(causalLearning)
-
 library(tidyverse)
+library(tools4uplift)
+
+
+set.seed(666)
 
 getwd()
-fashion_b <- read.csv("/Users/lukaskolbe/Library/Mobile Documents/com~apple~CloudDocs/UNI/Master/Applied Predictive Analytics/Data/books and toys/FashionB.csv", sep=",")
-fashion_b <- read.csv("/Users/Lukas/Library/Mobile Documents/com~apple~CloudDocs/UNI/Master/Applied Predictive Analytics/Data/books and toys/FashionB.csv", sep=",")
-
-str(fashion_b)
-
-table(fashion_b$controlGroup)
-
-fashion_b[,c(1:3)] <- NULL
-#fashion_b$campaignValue[fashion_b$campaignValue > 15] <- fashion_b$campaignValue[fashion_b$campaignValue > 15]/100 # correcting cases where discount precentages are given in two-digits instead of four (e.g. 15 instead of 1500)
+f_b <- read.csv("/Users/lukaskolbe/Library/Mobile Documents/com~apple~CloudDocs/UNI/Master/Applied Predictive Analytics/Data/fashion/FashionB.csv", sep=",")
+#f_b <- read.csv("/Users/Lukas/Library/Mobile Documents/com~apple~CloudDocs/UNI/Master/Applied Predictive Analytics/Data/fashion/FashionA.csv", sep=",")
+f_b <- read.csv("H:\\Applied Predictive Analytics\\Data\\fashion\\FashionA.csv", sep=",")
 
 
-table(fashion_b$campaignValue) #campaign value ALWAYS 500 CURRENCY UNITS, except for ~1000 where it is 0
-table(fashion_b$campaignValue, fashion_b$controlGroup)
+table(f_b$controlGroup)
+str(f_b)
 
-table(fashion_b$checkoutDiscount) #no checkout discounts in the data?!
-prop.table(table(fashion_b$checkoutAmount>0)) #9.3% have a positive checkout amount 
+table(f_b$campaignMov, f_b$campaignValue) # minimum order value is different depending on campaignValue (but consistent within value-segments)
+table(f_b$campaignValue) #campaign value mostly 2000 CURRENCY UNITS, except for ~66700
 
+with(f_b, prop.table(table(campaignValue,controlGroup), margin=1)) # proportions of control/treatment groups seem consistent across treatments
+summary(aov(campaignValue  ~ controlGroup, data=f_b)) 
+# there are three campaignValues with very different segment size, but they show no difference in treatment/control population >> deleting some data (campaignValue other than "2000" does not shift the distribution)
 
-table(fashion_b$controlGroup)
-with(fashion_b, prop.table(table(checkoutDiscount>0, controlGroup), margin=1)) # 25.5% of treatment group members (35533) received discount, 20.4% (7933) of control group members
-with(fashion_b, table(checkoutDiscount>0, controlGroup))
+#with(f_b, prop.table(table(converted,controlGroup, campaignValue), margin=1))
 
-treatment_uplift_b <- aggregate(checkoutAmount ~ controlGroup, data=fashion_b, mean)[1,2] - aggregate(checkoutAmount ~ controlGroup, data=fashion_b, mean)[2,2]
-treatment_uplift_b #the treatment gives an average uplift across the whole population of 0.4892036
-aov_b <- aov(checkoutAmount  ~ controlGroup, data=fashion_b)
-ttest_b <- t.test(checkoutAmount ~ controlGroup, data=fashion_b) # the differences in checkout amount are statistically significant!
-ttest_b
+table(f_b$checkoutDiscount) #no checkout discounts in the data?!
+prop.table(table(f_b$checkoutAmount>0, f_b$controlGroup)) #~5% have a positive checkout amount
 
-# feature engineering -----------------------------------------------------
+treatment_uplift_a <- aggregate(checkoutAmount ~ controlGroup, data=f_b, mean)[1,2] - aggregate(checkoutAmount ~ controlGroup, data=f_b, mean)[2,2]
+treatment_uplift_a #the treatment gives an average uplift across the whole population of 0.4892036
+summary(aov(checkoutAmount  ~ controlGroup, data=f_b)) # the differences in checkout amount are statistically significant!
+t.test(checkoutAmount ~ controlGroup, data=f_b) # the differences in checkout amount are statistically significant!
 
-#fashion_b$basketValue <- fashion_b$checkoutAmount + fashion_b$checkoutDiscount
-#fashion_b$discountPercentage  <- fashion_b$checkoutDiscount / fashion_b$basketValue
+names(f_b)
 
-#prop.table(table(fashion_b$basketValue>0)) # 15.4% have a positive basket value (sum of checkout amount and discount) >> this number should include all purchases!
+table(f_b$campaignMov)
 
-
-names(fashion_b)
-fashion_b <- fashion_b[,c(1:5, 60,8,9,6,7,10:59,61:90)]
-
-
-summary(fashion_b$checkoutDiscount)
-summary(fashion_b$discountPercentage)
-
-# Discount Delta Function -------------------------------------------------
-
-# THIS FUNCTION CALCULATES THE DIFFERENCE BETWEEN THE ACTUAL DISCOUNT AND THE DISCOUNT AS PRESCRIBED BY THE TREATMENT
-# Positive difference means the person reveiced MORE discount than offered in the treatmet, 
-# negative difference means the person received LESS discount 
-
-fashion_b$discountDelta <- numeric(nrow(fashion_b))
-condition1 <- (fashion_b$campaignUnit=="PERCENT" & fashion_b$controlGroup==0 & fashion_b$checkoutDiscount > 0)
-condition2 <- (fashion_b$campaignUnit=="CURRENCY" & fashion_b$controlGroup==0 & fashion_b$checkoutDiscount > 0)
-# pre-vectorization and allocation, and condition outsourcing taken from https://datascienceplus.com/strategies-to-speedup-r-code/
-
-for (i in seq_along(1:nrow(fashion_b))){
-  if (condition1[i]) {
-    fashion_b$discountDelta[i] <- fashion_b$checkoutDiscount[i] - (fashion_b$basketValue[i] * (fashion_b$campaignValue[i]/100))
-  }
-  else if(condition2[i]) {
-    fashion_b$discountDelta[i] <- fashion_b$checkoutDiscount[i] - fashion_b$campaignValue[i]
-  }
-}
-
-table(fashion_b$campaignUnit)
-table(fashion_b$campaignUnit, fashion_b$campaignValue)
+table(f_b$checkoutAmount>=5,f_b$controlGroup) #14.800 people qualified for actually using the discount of 20€ through achieving the minimum order value
+with(f_b, prop.table(table(checkoutAmount>=5,controlGroup), margin=1)) # 25% of the treatment group have a checkout amount >=105 and 22.8% of the control group do
 
 
-summary(fashion_b$discountDelta)
+f_b$treatment <- numeric(nrow(f_b))
+f_b$treatment <- ifelse(f_b$controlGroup==1, 0, 1)
 
-table(fashion_b$discountDelta>0,fashion_b$controlGroup)
-table(fashion_b$discountDelta<0, fashion_b$controlGroup)
+f_b <- f_b[,c(4:9,94,63,10:62,64:93,1,2,3)] # sorting new for better visibility of important columns
 
-table(fashion_b$discountPercentage>0.5,fashion_b$controlGroup, fashion_b$basketValue>50)
+#f_b_5 <- f_b[f_b$campaignValue==500,-which(names(f_b) %in% c("campaignUnit", "campaignTags", "trackerKey", "campaignId", "campaignValue"))] #separating the different treatment values
+#f_b_0 <- f_b[f_b$campaignValue==0,-which(names(f_b) %in% c("campaignUnit", "campaignTags", "trackerKey", "campaignId", "campaignValue"))]
+f_b <- f_b[f_b$campaignValue==2000,-which(names(f_b) %in% c("campaignUnit", "campaignTags", "trackerKey", "campaignId", "campaignValue"))] # only work with those with campaign-value of "2000" as they are the largest uniform group!
 
 
-#fashion_b_exclude <- fashion_b[(fashion_b$controlGroup==1 & fashion_b$checkoutDiscount > 0),]
-fashion_b <- fashion_b[!(fashion_b$controlGroup==1 & fashion_b$checkoutDiscount > 0),]
-fashion_b <- fashion_b[!(fashion_b$controlGroup==0 & fashion_b$discountDelta < 0),]
-fashion_b <- fashion_b[!(fashion_b$controlGroup==0 & fashion_b$discountDelta < 0),]
 
-hist(fashion_b$discountDelta[fashion_b$discountDelta>0], breaks=700)
+# stratification for production -------------------------------------------
 
+# 
+# train_indices_f_b <- list()
+# 
+# combinations <- expand.grid(list("Conversion"=c(0,1), "Treatment"= c(0,1))) # treatment is ordered 1,0 compared to hillström data because the variable indicates control group membership
+# xtabs(~converted+controlGroup, f_b)
+# sample_size_f_b <- as.numeric(xtabs(~converted+controlGroup, f_b))
+# 
+# 
+# for(i in 1:4){
+#   train_indices_f_b[[i]] <- sample(which(f_b$converted == combinations$Conversion[i] &
+#                                            f_b$controlGroup == combinations$Treatment[i])
+#                                    , size = round(0.75*sample_size_f_b[i]), replace=FALSE) 
+# } 
+# 
+# 
+# 
+# trainIndex_f_b <- c(train_indices_f_b, recursive=TRUE)
+# 
+# trainData <- f_b[trainIndex_f_b,]
+# testData  <- f_b[-trainIndex_f_b,]
+# 
+
+# 1ST ROUND SAMPLE SPLITTING AND STRATIFICATION ---------------------------------------------------
+
+train_indices_f_b <- list()
+
+combinations <- expand.grid(list("Conversion"=c(0,1), "Treatment"= c(0,1))) # treatment is ordered 1,0 compared to hillström data because the variable indicates control group membership
+xtabs(~converted+treatment, f_b)
+sample_size_f_b <- as.numeric(xtabs(~converted+treatment, f_b))
+
+
+for(i in 1:4){
+  train_indices_f_b[[i]] <- sample(which(f_b$converted == combinations$Conversion[i] &
+                                           f_b$treatment == combinations$Treatment[i])
+                                   , size = round(0.4*sample_size_f_b[i]), replace=FALSE) 
+} 
+
+
+
+trainIndex_f_b <- c(train_indices_f_b, recursive=TRUE)
+
+trainData_small <- f_b[trainIndex_f_b,] # temporarily the learning data is only a small partition!
+
+# SAMPLE SPLITTING AND STRATIFICATION 2ND ROUND (FOR PRE-TRAINING) ---------------------------------------------------
+
+train_indices_f_b2 <- list()
+
+#combinations_f_b2 <- expand.grid(list("Conversion"=c(0,1), "Treatment"= c(1,0))) # treatment is ordered 1,0 compared to hillström data because the variable indicates control group membership
+sample_size_f_b2 <- as.numeric(xtabs(~converted+treatment, trainData_small))
+
+
+for(i in 1:4){
+  train_indices_f_b2[[i]] <- sample(which(trainData_small$converted == combinations$Conversion[i] &
+                                            trainData_small$treatment == combinations$Treatment[i])
+                                    , size = round(0.25*sample_size_f_b2[i]), replace=FALSE) 
+} 
+
+
+
+trainIndex_f_b2 <- c(train_indices_f_b2, recursive=TRUE)
+
+trainData_f_b2 <- trainData_small[-trainIndex_f_b2,] # temporarily the train data is only a small partition!
+testData_f_b2  <- trainData_small[trainIndex_f_b2,]
+
+table(trainData_f_b2$checkoutAmount>0, trainData_f_b2$controlGroup)
+
+summary(aov(checkoutAmount  ~ controlGroup, data=trainData_f_b2)) # checking statistical significance of the differences in checkout amount
+t.test(checkoutAmount ~ controlGroup, data=trainData_f_b2) # checking statistical significance of the differences in checkout amount
+
+aggregate(checkoutAmount ~ controlGroup, data=f_b, mean)[1,2] - aggregate(checkoutAmount ~ controlGroup, data=f_b, mean)[2,2] 
+aggregate(checkoutAmount ~ controlGroup, data=trainData_f_b2, mean)[1,2] - aggregate(checkoutAmount ~ controlGroup, data=trainData_f_b2, mean)[2,2] 
+#total population uplift is slightly lower than in the complete sample
+
+
+
+# Average Treatment Effect (ATE) ---------------------------------------------------
+
+experiment <- table(list("Control" = trainData_f_b2$controlGroup, "Converted" = trainData_f_b2$converted))
+experiment
+
+# The ATE is the outcome difference between the groups, assuming that individuals in each group are similar
+# (((which is plausible because of the random sampling)))
+mean(as.numeric(trainData_f_b2$converted[trainData_f_b2$controlGroup==0])) - mean(as.numeric(trainData_f_b2$converted[trainData_f_b2$controlGroup==1]))
+mean(trainData_f_b2$checkoutAmount[trainData_f_b2$controlGroup==0]) - mean(trainData_f_b2$checkoutAmount[trainData_f_b2$controlGroup==1])
+
+# or alternatively:
+(experiment[1,2]/sum(experiment[1,]) ) - (experiment[2,2]/sum(experiment[2,]) )
+
+
+
+
+# DATA SAMPLE INVESTIGATION (DOES NOT WORK)-----------------------------------------------
+
+# Check whether the data has been randomly assigned. This is an important assumptions in uplift modeling and, more generally, experimental designs. 
+# To verify a random assignment, we have to check the balance of the A/B indicator.
+# The function checkBalance calculates standardized mean differences along each covariate and tests for conditional independence of the treatment variable and the covariates.
+# In randomized empirical experiments the treatment and control groups should be roughly similar (i.e. balanced) in their distributions of covariates.
+# Of course, we would expect the conversion rate to be different between the treatment and control group
+
+# n <- names(f_b)
+# f <- as.formula(paste("controlGroup ~", paste(n[!n %in% c("controlGroup","converted")], collapse = " + "))) # checkBalance() throws an error with the syntax (controlGroup ~.-converted), so I save the formula separately. doesn't help.
+# f
+# 
+# cb <- checkBalance(f, data = trainData_f_b2, report = c("adj.means", "adj.mean.diffs", "p.values", "chisquare.test"))
+# 
+# # Balance properties of first ten covariates 
+# # Be aware that the results are saved as a tensor or '3D matrix'.
+# dim(cb$results)
+# round(cb$results[,,], 2)
+# # The function automatically computes a chi-squared test for the conditional independence of the covariates to the treatment variable
+# cb$overall
+
+# CARET SMOTE SAMPLING TRYOUT ---------------------------------------------
+
+# ctrl <- trainControl(method = "repeatedcv", 
+#                      number = 10, 
+#                      repeats = 10,
+#                      verboseIter = FALSE,
+#                      sampling = "smote")
+# 
+# set.seed(42)
+# model_rf_smote <- caret::train(spend~recency + history +history_segment + mens + womens + zip_code + newbie + channel,
+#                                data = trainData_mens,
+#                                method = "glm",
+#                                preProcess = c("scale", "center"),
+#                                trControl = ctrl)
+# 
